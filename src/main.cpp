bf9bb1cc71ff2cccc20a8faf90d8e6530c472add
Mystical433 <peterpanlee2015@gmail.com>
2025-07-31 11:08:54 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 using namespace geode::prelude;
 
 class $modify(MyPlayLayer, PlayLayer) {
-    CCMenuItemSpriteExtra* m_levelInfoButton = nullptr;
+    struct Fields {
+        CCMenuItemSpriteExtra* m_levelInfoButton = nullptr;
+    };
     
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
@@ -19,45 +21,45 @@ class $modify(MyPlayLayer, PlayLayer) {
     void addLevelInfoButton() {
         auto buttonSprite = CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png");
         buttonSprite->setScale(0.8f);
-
-        m_levelInfoButton = CCMenuItemSpriteExtra::create(
+        auto levelInfoButton = CCMenuItemSpriteExtra::create(
             buttonSprite,
             this,
             menu_selector(MyPlayLayer::onLevelInfoButton)
         );
 
+        m_fields->m_levelInfoButton = levelInfoButton;
+
         auto winSize = CCDirector::sharedDirector()->getWinSize();
-        m_levelInfoButton->setPosition({50.0f, winSize.height - 50.0f});
+        levelInfoButton->setPosition({50.0f, winSize.height - 50.0f});
 
         auto menu = CCMenu::create();
-        menu->addChild(m_levelInfoButton);
+        menu->addChild(levelInfoButton);
         menu->setPosition({0, 0});
 
-        this->addChild(menu,9999);
+        this->addChild(menu, 6969);
     }
     
     void onLevelInfoButton(CCObject* sender) {
+
         auto level = this->m_level;
         if (!level) return;
-
+      
         auto infoLayer = LevelInfoLayer::create(level, false);
         if (infoLayer) {
             auto scene = CCDirector::sharedDirector()->getRunningScene();
             scene->addChild(infoLayer, 1000);
         }
     }
-
+ 
     void onQuit() {
         PlayLayer::onQuit();
     }
 };
-
 class $modify(MyLevelInfoLayer, LevelInfoLayer) {
     bool init(GJGameLevel* level, bool challenge) {
         if (!LevelInfoLayer::init(level, challenge)) {
             return false;
         }
-
         return true;
     }
 };
